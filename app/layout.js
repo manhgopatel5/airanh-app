e535bec7aa444ff91bf369f90b37d9f5e557c04a
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-04-10 15:19:01 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,53 @@
+"use client";
 import InstallPWA from "./components/InstallPWA";
-import ClientWrapper from "./components/ClientWrapper";
+import { useEffect, useState } from "react";
 
 export default function RootLayout({ children }) {
+  const [loading, setLoading] = useState(true);
+  const [hideSplash, setHideSplash] = useState(false);
+
+  useEffect(() => {
+    // hiển thị splash
+    const timer1 = setTimeout(() => {
+      setHideSplash(true); // bắt đầu fade out
+    }, 2000);
+
+    // xoá splash khỏi DOM
+    const timer2 = setTimeout(() => {
+      setLoading(false);
+    }, 2600);
+
+    return () => {
+      clearTimeout(timer1);
+      clearTimeout(timer2);
+    };
+  }, []);
+
   return (
-    <html lang="vi">
-      <head>
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#4aa3df" />
-      </head>
+    <html>
+      <body>
+        <>
+          {loading && <Splash hide={hideSplash} />}
+          
+          <div className={`app ${!loading ? "show" : ""}`}>
+            {children}
+          </div>
 
-      <body style={{ margin: 0, overflow: "hidden" }}>
-        <ClientWrapper>
-          {children}
-        </ClientWrapper>
+          <InstallPWA />
 
-        <InstallPWA />
+          <style jsx global>{`
+            .app {
+              opacity: 0;
+              transform: scale(1.02);
+              transition: all 0.6s ease;
+            }
+
+            .app.show {
+              opacity: 1;
+              transform: scale(1);
+            }
+          `}</style>
+        </>
       </body>
     </html>
   );
