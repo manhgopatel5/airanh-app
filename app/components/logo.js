cb2d4da5da0731c642dbcf42b710dd30d3e93016
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-04-10 17:36:00 +0700

    Update logo.js

--- app/components/logo.js
+++ app/components/logo.js
@@ -1,1 +1,11 @@
-
+export default function Logo({ size = 220 }) {
+  return (
+    <img
+      src="/logo-main.svg"
+      style={{
+        width: size,
+        height: "auto"
+      }}
+    />
+  );
+}
