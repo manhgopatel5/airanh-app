c1904b474d39b07455a2bd122bdd1f53142be29b
manhgopatel5 <77309108+manhgopatel5@users.noreply.github.com>
2026-04-10 17:46:12 +0700

    Create Logo.js

---
+++ app/components/Logo.js
@@ -0,0 +1,9 @@
+export default function Logo({ size = 260 }) {
+  return (
+    <div style={{ width: size }}>
+      <svg viewBox="0 0 360 220">
+        {/* toàn bộ SVG bạn đang có */}
+      </svg>
+    </div>
+  );
+}
